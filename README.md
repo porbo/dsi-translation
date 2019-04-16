1f6e460e2c4395655cf356c2d8f3690a007a03ba
John Peng <peng.j.1437@gmail.com>
2019-04-16 12:16:13 -0700

    control by country, and write simpler ttest function

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Spain's conversions are all considered "control", and spain in general had highe
 My thinking was that this unfairly increased the conversion rate in the control group, but even after excluding spain the test group had lower conversion.
 
 Quick t-test showed a pvalue around e-13. Very obviously not a random result.
+
+
+Noticed a bunch of people who aren't even using Spanish. Removed them from the test, but results didn't change.
+
+
+On the other hand, after filtering, each individual country seems to have insignificant difference in conversion with/without test.
+Maybe Argentina's high population and overall low conversion is messing things up?
+
+By removing Argentina as well, the conversion rate difference becomes insignificant.
--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scipy.stats as scs
 
 def conv_per_country(df):
     """
@@ -10,4 +11,27 @@ def conv_per_country(df):
         filtered = df[df['country'] == country]
         test = filtered[filtered['test'] == 1]
         control = filtered[filtered['test'] == 0]
-        print(country, ':\ttest:\t', test['conversion'].mean(), '\tcontrol:\t', control['conversion'].mean(), '\tcount:\t',filtered.shape[0])
+        print(country, ':\n\ttest:\t', round(test['conversion'].mean(),5), '\tcontrol:\t', round(control['conversion'].mean(),5), '\tcount:\t',filtered.shape[0], '\tp-value:\t', round(scs.ttest_ind(test['conversion'], control['conversion'])[1], 5))
+
+def df_filter(user, test):
+    """
+    input:
+        user: dataframe of user info
+        test: dataframe of info about the test
+    output: merged dataframe, limited to people outside of spain who use the spanish option
+    """
+    df = test.merge(user, on = 'user_id', how = 'left')
+    df2 = df[df['country'] != 'Spain']
+    return df2[df2['browser_language'] == 'ES']
+
+def ttest(df):
+    """
+    input: dataframe with a test column and a conversion column
+    output: ttest, test, control 
+        ttest:tuple results for conversion rates with and without the test 
+        test: array of conversion results in test group
+        control:array of conversion results in control group
+    """
+    test = df[df['test'] == 1]['conversion'].values
+    control = df[df['test'] == 0]['conversion'].values
+    return scs.ttest_ind(test, control), test, control
